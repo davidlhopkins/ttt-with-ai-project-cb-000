9198b3d60a124b66faac508c2d3224710d0c153b
David Hopkins <dlhopkins2@gmail.com>
2018-06-29 20:03:24 +0000

    automatically backed up by learn

--- lib/board.rb
+++ lib/board.rb
@@ -1,45 +1,45 @@
-class Board 
+class Board
   attr_accessor :cells, :board
 
-  def reset! 
-    @cells = Array.new(9, " ") 
-  end  
-  
+  def reset!
+    @cells = Array.new(9, " ")
+  end
+
   def initialize
     @cells = Array.new(9, " ")
-  end 
-  
-  def display 
+  end
+
+  def display
     puts " #{@cells[0]} | #{@cells[1]} | #{@cells[2]} "
     puts "-----------"
     puts " #{@cells[3]} | #{@cells[4]} | #{@cells[5]} "
     puts "-----------"
     puts " #{@cells[6]} | #{@cells[7]} | #{@cells[8]} "
-  end 
-  
+  end
+
   def position(num)
     @cells[num.to_i - 1]
   end
-  
-  def full? 
+
+  def full?
     @cells.none? {|c| c == " "}
   end
-  
-  def turn_count 
+
+  def turn_count
     count = @cells.select { |c| c != " "}
     count.size
   end
-  
+
   def taken?(num)
     position(num) != " "
-  end 
-  
+  end
+
   def valid_move?(num)
     !taken?(num) && (1..9) === num.to_i
   end
-  
+
   def update(num, player)
     @cells[num.to_i - 1] = player.token
-  end 
+  end
 
 end 
